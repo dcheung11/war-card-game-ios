205cd3f0240d4fe686900b065671e5137299c7d3
Damien Cheung <cheund11@mcmaster.ca>
2023-01-02 11:02:57 -0500

    Initial Commit

---
+++ War-App/War_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_AppApp.swift
+//  War-App
+//
+//  Created by Damien Cheung on 2023-01-02.
+//
+
+import SwiftUI
+
+@main
+struct War_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
